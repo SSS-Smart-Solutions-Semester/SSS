6a5c674a48fb324997fd6029ab56951e2fe3026c
George <georgeantonio1998@yahoo.com>
2020-04-16 11:37:32 +0200

    Unity : Small prototype to show functionality

--- SSS Project/Assets/Scripts/LineRendererDistance.cs
+++ SSS Project/Assets/Scripts/LineRendererDistance.cs
@@ -1,33 +1,33 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
-namespace Assets.Scripts
+public class LineRendererDistance : MonoBehaviour
 {
-    public class LineRendererDistance : MonoBehaviour
-    {
 
-        [SerializeField] protected LineRenderer _lineRend;
+    [SerializeField] protected LineRenderer lineRend;
 
-        [SerializeField] protected GameObject _position1;
-        [SerializeField] protected GameObject _position2;
+    [SerializeField] protected GameObject position1;
+    [SerializeField] protected GameObject position2;
+
+    protected float Round;
         
-        protected LineRendererDistance(GameObject position1, GameObject position2)
-        {
-            _position1 = position1;
-            _position2 = position2;
-        }
+    protected LineRendererDistance(GameObject position1, GameObject position2)
+    {
+        this.position1 = position1;
+        this.position2 = position2;
+    }
 
-        private void Start()
-        {
-            _lineRend = GetComponent<LineRenderer>();
-            _lineRend.positionCount = 2;
-        }
+    private void Start()
+    {
+        lineRend = GetComponent<LineRenderer>();
+        lineRend.positionCount = 2;
+            
+        lineRend.startColor = Color.green;
+        lineRend.endColor = Color.green;
+    }
 
-        private void Update()
-        {
-            _lineRend.SetPosition(0,_position1.transform.position);
-            _lineRend.SetPosition(1,_position2.transform.position);
-        }
+    private void Update()
+    {
+        lineRend.SetPosition(0, position1.transform.position);
+        lineRend.SetPosition(1, position2.transform.position);
     }
 }
--- SSS Project/Assets/Scripts/LuggageSize.cs
+++ SSS Project/Assets/Scripts/LuggageSize.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
-using Assets.Scripts;
+﻿using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LuggageSize : LineRendererDistance
 {
     [SerializeField] private Text text;
+    
 
     private void Update()
     {
-        float round = Mathf.Round((_position1.transform.position - _position2.transform.position).magnitude * 10f) / 10f;
+        Round = Mathf.Round((position1.transform.position - position2.transform.position).magnitude * 10f) / 10f;
         
-        text.text = round.ToString(CultureInfo.InvariantCulture) + " CM";
+        text.text = Round.ToString(CultureInfo.InvariantCulture) + " CM";
     }
-    
-    public LuggageSize(GameObject position1, GameObject position2) : base(position1, position2)
-    {
-        
-    }
+
+    protected LuggageSize(GameObject position1, GameObject position2) : base(position1, position2){}
 }
